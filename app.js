c46e7c3c228f7d4042ce224984c583357f938047
komarite <ulasgulhan9@gmail.com>
2023-07-28 22:00:34 +0300

    /movies/between end point

--- app.js
+++ app.js
@@ -3,11 +3,15 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const bodyParser = require('body-parser');
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const movie = require('./routes/movie');
 
 const app = express();
+
+//db connection
+const db = require('./helper/db.js')();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -15,12 +19,12 @@ app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api/movies', movie);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
@@ -35,7 +39,7 @@ app.use((err, req, res, next) => {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.json({ error: { message: err.message, code: err.code} });
 });
 
 module.exports = app;
