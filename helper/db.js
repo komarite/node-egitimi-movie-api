ced85a50159a31bac1fb90897dae94ff096b9d70
komarite <ulasgulhan9@gmail.com>
2023-07-30 20:07:46 +0300

    test yazılması

--- helper/db.js
+++ helper/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 module.exports = () => {
     mongoose.connect('mongodb+srv://movie-user:abcd1234@cluster0.sfmeh64.mongodb.net/?retryWrites=true&w=majority')
     mongoose.connection.on('open', () => {
-        console.log('MongoDB: Connected');
+        //console.log('MongoDB: Connected');
     });
     mongoose.connection.on('error', (err) => {
         console.log('MongoDB: Error', err);
